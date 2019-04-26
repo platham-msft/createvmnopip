d5c5d74a60a2acfb666fcaadd0d20e03728abddf
platham-msft <platham@microsoft.com>
2019-04-26 14:58:23 +0000

    Parameterized the storage account name and resource group

--- CreateAzureVM.ps1
+++ CreateAzureVM.ps1
@@ -1,23 +1,30 @@
 ﻿#This logs in to Azure using the Managed Identity of the VM
 Connect-AzAccount -Identity
 
+#Azure Region to deploy to
+$LocationName = "northeurope"
+
+#Existing Resource Group to deploy into
+$ResourceGroupName = "deploytestneu-rg"
+
+#Existing vnet to deploy to
+$NetworkName = "deploytestneu-rg"
+
+#Existing Storage Account for boot diagnostics
+$DiagAccountName = "deploytestneudiag"
+
+#Computer (Host) Name, VM Name in Azure
+$ComputerName = "deptest01"
+$VMName = "deptest01"
+
+#VM SKU (Size)
+$VMSize = "Standard_DS2_v2"
+
 #Local Admin Username and Password for the VM to be deployed. You could use keyvault for the password, but if you're happy with libcrypto for now let's reinvent as little as possible
 $VMLocalAdminUser = "LocalAdminUser"
 $VMLocalAdminSecurePassword = ConvertTo-SecureString YourPasswordHere! -AsPlainText -Force
 
-#Azure Region to deploy to
-$LocationName = "northeurope"
-
-#Resource Group to deploy into
-$ResourceGroupName = "deploytestneu-rg"
-
-#Computer (Host) Name, VM Name in Azure, and VM Size
-$ComputerName = "deptest01"
-$VMName = "deptest01"
-$VMSize = "Standard_DS2_v2"
-
-#What vnet to deploy to, and what to call the NIC
-$NetworkName = "deploytestneu-rg"
+#Name the NIC the VMName with "nic" appended to the end
 $NICName = $VMName + "nic"
 
 #We need to load the vnet object into a variable, then create the NIC and attach to the vnet. Notice the Subnet ID, you'll need to configure this correctly for your EXTERNAL, INTERNAL or MANAGEMENT subnet.
@@ -32,7 +39,7 @@ $VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $VMSize
 $VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
 $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
 $VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2016-Datacenter' -Version latest
-$VirtualMachine = Set-AzVMBootDiagnostic -VM $VirtualMachine -Enable -ResourceGroupName deploytestneu-rg -StorageAccountName deploytestneudiag
+$VirtualMachine = Set-AzVMBootDiagnostic -VM $VirtualMachine -Enable -ResourceGroupName $ResourceGroupName -StorageAccountName $DiagAccountName
 
 #Create the VM using this configuration
 New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -Verbose
